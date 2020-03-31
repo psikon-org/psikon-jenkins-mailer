c58ed7bf83c4a0d100f2322728159257c62f8fe6
Alex Dow <adow@psikon.com>
2020-03-31 08:33:01 -0400

    first iteration of mailer

---
+++ vars/psikonMailer.groovy
@@ -0,0 +1,36 @@
+def signatures = """
+  _______   _______   ___   ___ ___    _______   ______
+ |   _   | |   _   | |   | |   Y   )  |   _   | |   _  \
+ |.  1   | |   1___| |.  | |.  1  /   |.  |   | |.  |   |
+ |.  ____| |____   | |.  | |.  _  \   |.  |   | |.  |   |
+ |:  |     |:  1   | |:  | |:  |   \  |:  1   | |:  |   |
+ |::.|     |::.. . | |::.| |::.| .  ) |::.. . | |::.|   |
+ `---'     `-------' `---' `--- ---'  `-------' `--- ---'
+
+                  https://psikon.org
+"""
+
+def call() {
+  script {
+    def subject = ""
+    def body    = ""
+    subject = "[psikon-ci] ${env.JOB_NAME} (${env.BUILD_NUMBER}) - ${currentBuild.result}!"
+
+    body = """${env.JOB_NAME} (${env.BUILD_NUMBER}) has finished with status ${currentBuild.result}.
+
+Duration: ${currentBuild.durationString}
+URL     : ${currentBuild.absoluteUrl}
+
+${signatures}"""
+
+    env.PSIKON_EMAIL_BODY = body;
+    env.PSIKON_EMAIL_SUBJECT = subject;
+
+  }
+
+  emailext(
+    subject: env.PSIKON_EMAIL_SUBJECT,
+    body: env.PSIKON_EMAIL_BODY,
+    to: "ci@psikon.com"
+  )
+}
